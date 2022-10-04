c7e5636095f2c7c8a8ae5c8df9b8c6b139acd75e
dkkim <ekakth2458@gmail.com>
2022-10-04 22:05:44 +0900

    TodoListItem 만들기

---
+++ todo-app/src/components/TodoListItem.js
@@ -0,0 +1,24 @@
+import{
+    MdCheckBoxOutlineBlank, 
+    MdCheckBox, 
+    MdRemoveCircleOutline
+} from 'react-icons/md'
+import React from 'react';
+import './TodoListItem.scss'
+const TodoListItem = () => {
+    return (
+        <div className='TodoListItem'>
+            <div className='checkbox'>
+                <MdCheckBoxOutlineBlank/>
+                <div className='text'>
+                    할 일
+                </div>
+            </div>
+            <div className='remove'>
+                <MdRemoveCircleOutline></MdRemoveCircleOutline>
+            </div>         
+        </div>
+    );
+};
+
+export default TodoListItem;
